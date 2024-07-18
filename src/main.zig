cbf0c52bdec974310af1de95fc22062460c22c07
hronro <hronro@gmail.com>
2024-07-18 16:44:55 +0800

    :fire: remove dead code

--- src/main.zig
+++ src/main.zig
@@ -2,10 +2,6 @@ const std = @import("std");
 
 const getConfigFromCli = @import("./config.zig").getConfigFromCli;
 const run = @import("./run.zig").run;
-
-const HELP_TEXT = @embedFile("./help.txt");
-const VERSION = "0.0.1";
-
 var general_purpose_allocator = std.heap.GeneralPurposeAllocator(.{}){};
 
 pub fn main() !void {
